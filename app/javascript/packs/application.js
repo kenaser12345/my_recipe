684613cc6408604939fd594c7dbab29b2078fd13
Ken <kenaser12345@gmail.com>
2021-05-13 21:30:22 +0800

    solve select2 do not worked

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,21 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "bootstrap"
-
-
-
-require("styles")
-require("bootstrap")
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap";
+import "controllers";
 import $ from 'jquery'
+
+import select2 from 'select2'
+import 'select2/dist/css/select2.css'
 window.$ = $
-import "controllers"
+$(document).ready(function() {
+  $('select').select2();
+});
+
+require("styles")
+require("bootstrap")
+require("select2")
+
+
+
+
